7d4e7c7614235cd4cee1555aa70889d934b7f139
Kafilat <kafilatadewumi61@gmail.com>
2023-04-10 07:02:12 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=nigeria&appid=8ef61f42b5a51741d9850cc772f4beee";
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Nigeria</p>
+          </div>
+          <div className='temp'>
+            <h1>60<sup>o</sup>F</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65<sup>o</sup>F</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+{
+"coord": {
+"lon": 8,
+"lat": 10,
+},
+"weather": [
+{
+"id": 804,
+"main": "Clouds",
+"description": "overcast clouds",
+"icon": "04d"
+}
+],
+"base": "stations",
+"main": {
+"temp": 310.76,
+"feels_like": 308.75,
+"temp_min": 310.76,
+"temp_max": 310.76,
+"pressure": 1005,
+"humidity": 17,
+"sea_level": 1005,
+"grnd_level": 923
+},
+"visibility": 10000,
+"wind": {
+"speed": 1.83,
+"deg": 284,
+"gust": 3.42
+},
+"clouds": {
+"all": 100
+},
+"dt": 1680870736,
+"sys": {
+"country": "NG",
+"sunrise": 1680844914,
+"sunset": 1680889091
+},
+"timezone": 3600,
+"id": 2328926,
+"name": "Nigeria",
+"cod": 200,
+}
